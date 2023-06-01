373305bad9257b1f56dc992d84b657c83e4d4b8e
Eather <eathertoo@gmail.com>
2023-06-01 23:13:41 +0800

    feat: home page migration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ]
+  },
+  images: {
+    domains: ['revery-integration-tools.s3.amazonaws.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/404',
+        destination: '/index.html',
+        permanent: false,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
