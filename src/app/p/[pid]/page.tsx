981e42f72195f7ffc7b025c40884ade6749791ae
Eather <eathertoo@gmail.com>
2023-06-09 15:38:20 +0800

    fix: meta data更新

--- src/app/p/[pid]/page.tsx
+++ src/app/p/[pid]/page.tsx
@@ -1,75 +1,49 @@
-import DownLoadBar from '@components/DownLoadBar/DownLoadBar'
-import BackButton from '@components/BackButton/BackButton'
-import PostBigImage from '@components/PostBigImage/PostBigImage'
 import postService from '@services/postPage.service'
-import withS3Prefix from '@utils/withS3Prefix'
-import dayjs from 'dayjs'
-import ProductImage from './_components/ProductImage'
-import Link from 'next/link'
-import Head from 'next/head'
+import { Metadata, ResolvingMetadata } from 'next'
 import PostPage from './PostPage'
 type PostPageProps = {
   params: { pid: string }
+}
+
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const { pid } = params
+  const { post } = await postService.getPostInfos(pid)
+  let story_preview = 'https://media.revery.ai/media_assets/' + pid + '.jpg'
+
+  let twitter_story_preview =
+    'https://media.revery.ai/media_assets/' + pid + '_twitter_cropped.jpg'
+  const res = await fetch(twitter_story_preview, { method: 'HEAD' })
+  if (!res.ok) {
+    twitter_story_preview = story_preview
+  }
+
+  const username = post.creator_profile.username
+  return {
+    openGraph: {
+      title: username
+        ? username + ' on Reverie'
+        : 'Download Reverie - Edit looks, explore fashion, and more!',
+      description: 'Edit looks, explore fashion, and more!',
+      images: [story_preview],
+      type: 'website',
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: username
+        ? username + ' on Reverie' + ' on Reverie'
+        : 'Download Reverie - Edit looks, explore fashion, and more!',
+      description: 'Edit looks, explore fashion, and more!',
+      images: [twitter_story_preview],
+    },
+  }
 }
 
 export default async function PostPageWrapper(props: PostPageProps) {
   const { pid } = props.params
   const { post } = await postService.getPostInfos(pid)
 
-  let story_preview = 'https://media.revery.ai/media_assets/' + pid + '.jpg'
-  let twitter_story_preview =
-    'https://media.revery.ai/media_assets/' + pid + '_twitter_cropped.jpg'
-  let username = ''
-  const res = await fetch(twitter_story_preview, { method: 'HEAD' })
-  if (!res.ok) {
-    twitter_story_preview = story_preview
-  }
-  await fetch('https://stylespace.revery.ai/get_story_by_id?id=' + pid, {
-    method: 'GET',
-  })
-    .then((response) => response.json())
-    .then((body) => {
-      if (body.success) {
-        username = body.published_story.creator_profile.username
-      }
-    })
-
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {username != '' ? (
-          <meta property="og:title" content={username + ' on Reverie'} />
-        ) : (
-          <meta
-            property="og:title"
-            content="Download Reverie - Edit looks, explore fashion, and more!"
-          />
-        )}
-        <meta
-          property="og:description"
-          content="Edit looks, explore fashion, and more!"
-        />
-        <meta property="og:image" content={story_preview} />
-        <meta property="og:image:secure_url" content={story_preview} />
-        <meta content="summary_large_image" name="twitter:card" />
-        {username != '' ? (
-          <meta property="twitter:title" content={username + ' on Reverie'} />
-        ) : (
-          <meta
-            content="Download Reverie - Edit looks, explore fashion, and more!"
-            property="twitter:title"
-          />
-        )}
-        <meta
-          content="Edit looks, explore fashion, and more!"
-          property="twitter:description"
-        />
-        <meta content={twitter_story_preview} property="twitter:image" />
-        <meta property="og:type" content="website" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <PostPage pid={pid} post={post} />
-    </>
-  )
+  return <PostPage pid={pid} post={post} />
 }
--- src/app/u/[uid]/page.tsx
+++ src/app/u/[uid]/page.tsx
@@ -1,8 +1,38 @@
 import profileService from '@services/profile.service'
 import UserProfile from './UserProfile'
 import Head from 'next/head'
+import { Metadata } from 'next'
 type UserProfileProps = {
   params: { uid: string }
+}
+
+export async function generateMetadata({
+  params,
+}: UserProfileProps): Promise<Metadata> {
+  const { uid } = params
+
+  const { profile } = await profileService.getProfile(uid)
+
+  const username = profile.username
+  return {
+    openGraph: {
+      title: username
+        ? username + ' on Reverie'
+        : 'Download Reverie - Edit looks, explore fashion, and more!',
+      description: 'Edit looks, explore fashion, and more!',
+      images: [profile.profile_url],
+      type: 'website',
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: username
+        ? username + ' on Reverie' + ' on Reverie'
+        : 'Download Reverie - Edit looks, explore fashion, and more!',
+      description: 'Edit looks, explore fashion, and more!',
+      images: [profile.profile_url],
+    },
+  }
 }
 
 export default async function UserProfilePage(props: UserProfileProps) {
