9e56cd4e8ebb649fac951b9902334f9d64d9055a
Eather <eathertoo@gmail.com>
2023-06-09 16:05:26 +0800

    fix: update deeplink text

--- src/app/_components/DownLoadBar/DownLoadBar.tsx
+++ src/app/_components/DownLoadBar/DownLoadBar.tsx
@@ -36,17 +36,16 @@ export default function DownLoadBar(props: DownLoadBarProps) {
           style={{
             borderImage: 'linear-gradient(165deg, #b4b4b9, #3b3b40) 1',
           }}
-          className="w-129 h-32 lg:w-167 lg:h-47 pt-5 pl-18 bg-[#E4E7EF] border-solid border-[1px] lg:border-[1px] flex shadow-[inset_-1px_-1px_1px_0px_#00000060] lg:shadow-[inset_-1px_-1px_1px_0px_#00000060]"
+          className="w-129 h-32 lg:w-167 lg:h-47 pt-5 pl-12 bg-[#E4E7EF] border-solid border-[1px] lg:border-[1px] flex shadow-[inset_-1px_-1px_1px_0px_#00000060] lg:shadow-[inset_-1px_-1px_1px_0px_#00000060]"
         >
           <Image
-            className="w-16 h-19 lg:w-24 lg:h-28 shrink-0"
+            className="w-16 h-19 scale-90 lg:scale-[none] lg:w-24 lg:h-28 shrink-0 lg:mt-2"
             src={AppleLogoImg}
             alt="Apple logo"
           />
-          <div className="text-[10px] lg:text-[15px] lg:leading-[18px] ml-8 shrink-0 text-[#434348] leading-[12px]">
-            <p>DOWNLOAD</p>
-            <p className="text-[8px] leading-[8px] lg:text-[12px] lg:leading-[14px]">
-              ON APP STORE
+          <div className="text-center ml-6 shrink-0 text-[#434348] mt-7 lg:mt-10">
+            <p className="text-[14px] leading-[8px] lg:text-[17px] lg:leading-[14px]">
+              View In App
             </p>
           </div>
         </div>
